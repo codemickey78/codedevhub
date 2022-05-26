65e351dae7f8cf26403f92749edc0cef0242da12
codemickey78 <codemickey78@gmail.com>
2022-05-26 13:34:01 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import './App.scss';
+import App from "./App";
+
+const mainElem = document.getElementById("root");
+const root = createRoot(mainElem);
+
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
